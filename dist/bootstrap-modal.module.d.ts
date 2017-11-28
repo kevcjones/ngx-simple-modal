a0e464a20668860c3855825939589b675435591d
Kevin Jones <kevin.jones@nucleuscentral.com>
2017-11-28 21:01:34 +0000

    rebuilt in a different node js and npm version - seemed to work

--- dist/bootstrap-modal.module.d.ts
+++ dist/bootstrap-modal.module.d.ts
@@ -1,2 +1,5 @@
+import { ModuleWithProviders } from '@angular/core';
+import { DialogServiceConfig } from './dialog.service';
 export declare class BootstrapModalModule {
+    static forRoot(config: DialogServiceConfig): ModuleWithProviders;
 }
