494ea80667c144c4a86a047c0fbc8208c8c3dbe1
Kevin Jones <kevin.jones@nucleuscentral.com>
2017-11-28 14:42:30 +0000

    need help with an error

--- dist/bootstrap-modal.module.js
+++ dist/bootstrap-modal.module.js
@@ -5,23 +5,9 @@ var core_1 = require("@angular/core");
 var dialog_holder_component_1 = require("./dialog-holder.component");
 var dialog_wrapper_component_1 = require("./dialog-wrapper.component");
 var dialog_service_1 = require("./dialog.service");
-var dialog_service_factory_1 = require("./dialog-service.factory");
 var BootstrapModalModule = (function () {
     function BootstrapModalModule() {
     }
-    BootstrapModalModule.forRoot = function (config) {
-        return {
-            ngModule: BootstrapModalModule,
-            providers: [
-                { provide: dialog_service_1.DialogServiceConfig, useValue: config },
-                {
-                    provide: dialog_service_1.DialogService,
-                    useFactory: dialog_service_factory_1.dialogServiceFactory,
-                    deps: [core_1.ComponentFactoryResolver, core_1.ApplicationRef, core_1.Injector, dialog_service_1.DialogServiceConfig]
-                }
-            ]
-        };
-    };
     return BootstrapModalModule;
 }());
 BootstrapModalModule.decorators = [
