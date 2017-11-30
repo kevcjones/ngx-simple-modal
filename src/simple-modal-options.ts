2da219fe76e9a8b608da110c7a74ee3db6e6b82b
Kevin Jones <kevin.jones@nucleuscentral.com>
2017-11-30 02:09:44 +0000

    removed old, added some new options + defaults obj

--- src/simple-modal-options.ts
+++ src/simple-modal-options.ts
@@ -1,6 +1,20 @@
 export interface SimpleModalOptions {
   index?: number;
+  keyboard?: boolean;
+  hideOthers?: boolean;
   autoCloseTimeout?: number;
   closeByClickingOutside?: boolean;
-  backdropColor?: string;
+  bodyClass?: string;
+  wrapperClass?: string;
+  animationDuration?: number;
 }
+
+export const defaultModalOptions: SimpleModalOptions = {
+  keyboard: false,
+  hideOthers: false,
+  autoCloseTimeout: -1,
+  closeByClickingOutside: false,
+  bodyClass: 'modal-open',
+  wrapperClass: 'in',
+  animationDuration: 300
+};
