a580656b3649330bee80bd4a58b5c836617afda9
CelsoTrindade <celsomtrindade@hotmail.com>
2017-12-18 10:12:25 -0200

    Update demo to use the new styles

--- demo/src/app/confirm/confirm.component.ts
+++ demo/src/app/confirm/confirm.component.ts
@@ -2,27 +2,26 @@ import { Component } from '@angular/core';
 import { SimpleModalComponent } from 'ngx-simple-modal';
 
 export interface ConfirmModel {
-  title:string;
-  message:string;
+  title: string;
+  message: string;
 }
 
 @Component({
   selector: 'confirm',
-  template: `<div class="modal-dialog">
-                <div class="modal-content">
-                   <div class="modal-header">
-                     <button type="button" class="close" (click)="close()" >&times;</button>
-                     <h4 class="modal-title">{{title || 'Confirm'}}</h4>
-                   </div>
-                   <div class="modal-body">
-                     <p>{{message || 'Are you sure?'}}</p>
-                   </div>
-                   <div class="modal-footer">
-                     <button type="button" class="btn btn-primary" (click)="confirm()">OK</button>
-                     <button type="button" class="btn btn-default" (click)="cancel()">Cancel</button>
-                   </div>
-                 </div>
-                </div>`
+  template: `
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4>{{title || 'Confirm'}}</h4>
+      </div>
+      <div class="modal-body">
+        <p>{{message || 'Are you sure?'}}</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-outline-danger" (click)="cancel()">Cancel</button>
+        <button type="button" class="btn btn-primary" (click)="confirm()">OK</button>
+      </div>
+    </div>
+  `
 })
 export class ConfirmComponent extends SimpleModalComponent<ConfirmModel, boolean> implements ConfirmModel {
   title: string;
