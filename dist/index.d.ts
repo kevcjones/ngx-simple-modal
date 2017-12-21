ae679da3d1cdc3f5ee648a722f43f9e29cca8375
Kevin Jones <him@kevincjones.co.uk>
2017-12-21 21:07:25 +0000

    remove dist from github, only needed for publishing

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { SimpleModalModule } from './simple-modal/simple-modal.module';
-export { SimpleModalComponent } from './simple-modal/simple-modal.component';
-export { SimpleModalOptions } from './simple-modal/simple-modal-options';
-export { SimpleModalService, SimpleModalServiceConfig } from './simple-modal/simple-modal.service';
--- dist/simple-modal/simple-modal-service.factory.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var simple_modal_service_1 = require("./simple-modal.service");
-function SimpleModalServiceFactory(resolver, applicationRef, injector, options) {
-    return new simple_modal_service_1.SimpleModalService(resolver, applicationRef, injector, options);
-}
-exports.SimpleModalServiceFactory = SimpleModalServiceFactory;
-//# sourceMappingURL=simple-modal-service.factory.js.map
