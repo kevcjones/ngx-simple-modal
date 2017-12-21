db8cd186b45d072a5d8b1d31ec1341d119025398
Kevin Jones <him@kevincjones.co.uk>
2017-12-21 19:37:40 +0000

    updated test branch to contain latest

--- dist/simple-modal-service.factory.d.ts
+++
@@ -1,3 +0,0 @@
-import { ApplicationRef, ComponentFactoryResolver, Injector } from '@angular/core';
-import { SimpleModalService, SimpleModalServiceConfig } from './simple-modal.service';
-export declare function SimpleModalServiceFactory(resolver: ComponentFactoryResolver, applicationRef: ApplicationRef, injector: Injector, options: SimpleModalServiceConfig): SimpleModalService;
--- dist/simple-modal.module.d.ts
+++
@@ -1,5 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-import { SimpleModalServiceConfig } from './simple-modal.service';
-export declare class SimpleModalModule {
-    static forRoot(config: SimpleModalServiceConfig): ModuleWithProviders;
-}
