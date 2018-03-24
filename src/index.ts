44bd1d0d3d4006849bf0c9c5209f2f597b618367
Kevin Jones <kevin.jones@pushdr.com>
2018-03-24 11:55:14 +0000

    expose the default config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { SimpleModalModule } from './simple-modal/simple-modal.module';
 export { SimpleModalComponent } from './simple-modal/simple-modal.component';
-export { SimpleModalOptions }  from './simple-modal/simple-modal-options';
+export { SimpleModalOptions, DefaultSimpleModalOptionConfig }  from './simple-modal/simple-modal-options';
 export { SimpleModalService, SimpleModalServiceConfig } from './simple-modal/simple-modal.service';
