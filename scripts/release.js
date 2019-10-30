56914755b60ed5de3145d281bbf9a7d077fcc4e5
Alan Agius <alan.agius4@gmail.com>
2019-10-30 10:35:25 +0100

    build: use ng-packagr to follow APF specs
    In version 9, Angular will enable Ivy by default, currently this package is not compatible with Ivy because NGCC is unable to run successfully as it doesn't follow the APF (Angular Package Format) spec https://docs.google.com/document/d/1CZC2rcpxffTDfRDs6p1cfbmKNLA6x5O-NtkJglDaBVs/preview
    
    With this change we replace the build steps to use ng-packagr so that the distributable can be interpreted by NGCC.

--- scripts/release.js
+++ scripts/release.js
@@ -1,38 +1,8 @@
-const del = require('del');
 const fs = require('fs-extra');
-const package = require('../package.json');
-
-delete package.scripts;
-delete package.devDependencies;
-
-package.main = patchPath(package.main);
-package.module = patchPath(package.module);
-package.typings = patchPath(package.typings);
 
 try {
-  fs.writeJsonSync('dist/package.json', package, { spaces: '   ' });
-  console.log('package.json was written');
-} catch (e) {
-  console.error(`Failed to write package.json file due to: ${e}`);
-}
-
-try {
-  fs.copy('README.md', 'dist/README.md');
-  fs.copy('LICENSE.md', 'dist/LICENSE.md');
   fs.copy('src/styles', 'dist/styles');
   console.log('Copied additional files');
 } catch (e) {
   console.error(`Failed to copy additional files due to: ${e}`);
-}
-
-del('dist/tests', { force: true })
-  .then(paths => {
-    console.log('deleted tests folder..');
-  })
-  .catch(e => {
-    console.error(`Failed to delete test folder from final package due to: ${e}`);
-  });
-
-function patchPath(path) {
-  return path.replace('dist/', '');
 }
